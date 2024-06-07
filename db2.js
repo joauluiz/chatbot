ff6a51e5a1bb3d9aa32da972b5b7cfc8fc666e9e
JOAO LUIZ MIRANDA MEYER <74273557+joauluiz@users.noreply.github.com>
2024-06-07 14:12:28 -0300

    Add files via upload

---
+++ db2.js
@@ -0,0 +1,29 @@
+import pg from 'pg'
+const { Client } = pg
+
+export function InserirDadoDb(json){
+const Clien = new Client({
+
+    host: 'localhost',
+    user: 'postgres',
+    port: 5432,
+    password: "",
+    database: 'postgres'
+
+
+})
+
+Clien.connect();
+
+Clien.query("DO $$ BEGIN IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'main_table') THEN CREATE TABLE main_table (id SERIAL PRIMARY KEY, data JSON NOT NULL); END IF; END $$;");
+
+let jsonString = json
+
+var quer='INSERT INTO main_table (data) VALUES ($1);'
+
+Clien.query(quer, [jsonString])
+
+
+}
+
+//InserirDadoDb('json')
